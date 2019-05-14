752d98f0c0e34b62bfaf2f11d1f1c6df221cf271
FaridC <48392059+FaridC@users.noreply.github.com>
2019-05-13 20:46:09 -0500

    Se realizó "Transformar FloatingActionButton en Toolbar" Un cambio en actividad_principal.xml para agregar los iconos y el FabToolBar, ActividadPrincipal.java modificar eventos del FloatingActionButton (Botón) e importar la librería FabToolBar desde el gradle

--- app/src/main/java/com/example/floatingactionbutton/ActividadPrincipal.java
+++ app/src/main/java/com/example/floatingactionbutton/ActividadPrincipal.java
@@ -1,21 +1,18 @@
 package com.example.floatingactionbutton;
 
-import android.animation.Animator;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.animation.AnimationUtils;
-import android.view.animation.Interpolator;
 
-public class ActividadPrincipal extends AppCompatActivity {
+import com.github.fafaldo.fabtoolbar.widget.FABToolbarLayout;
 
-    boolean click = false;
+public class ActividadPrincipal extends AppCompatActivity implements View.OnClickListener {
+
+    private FABToolbarLayout morph;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,83 +21,28 @@ public class ActividadPrincipal extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        final FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setImageResource(android.R.drawable.ic_menu_add);
-        /*fab.setScaleX(1);
-        fab.setScaleY(1);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        morph = (FABToolbarLayout) findViewById(R.id.fabtoolbar);
 
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
-            final Interpolator interpolator = AnimationUtils.loadInterpolator(getBaseContext(), android.R.interpolator.fast_out_slow_in);
-            fab.animate()
-                    .scaleX(1)
-                    .scaleY(1)
-                    .setInterpolator(interpolator)
-                    .setDuration(600)
-                    .setStartDelay(1000)
-                    .setListener(new Animator.AnimatorListener() {
-                        @Override
-                        public void onAnimationStart(Animator animation) {
+        View uno, dos, tres, cuatro;
+        uno = findViewById(R.id.uno);
+        dos = findViewById(R.id.dos);
+        tres = findViewById(R.id.tres);
+        cuatro = findViewById(R.id.cuatro);
 
-                        }
-
-                        @Override
-                        public void onAnimationEnd(Animator animation) {
-                            fab.animate()
-                                    .scaleY(0)
-                                    .scaleX(0)
-                                    .setInterpolator(interpolator)
-                                    .setDuration(600)
-                                    .start();
-                        }
-
-                        @Override
-                        public void onAnimationCancel(Animator animation) {
-
-                        }
-
-                        @Override
-                        public void onAnimationRepeat(Animator animation) {
-
-                        }
-                    });
-        }*/
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                click = !click;
-                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
-                    Interpolator interpolator = AnimationUtils.loadInterpolator(getBaseContext(), android.R.interpolator.fast_out_slow_in);
-
-                    view.animate()
-                            .rotation(click ? 45f : 0)
-                            .setInterpolator(interpolator)
-                            .start();
-                }
-                /*Snackbar.make(view, "Se presionó el FAB", Snackbar.LENGTH_LONG).
-                        .setAction("Action", null).show();*/
-            }
-        });
+        fab.setOnClickListener(this);
+        uno.setOnClickListener(this);
+        dos.setOnClickListener(this);
+        tres.setOnClickListener(this);
+        cuatro.setOnClickListener(this);
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    public void onClick(View v){
+        if (v.getId() == R.id.fab){
+            morph.show();
         }
-
-        return super.onOptionsItemSelected(item);
+        morph.hide();
     }
+
 }
